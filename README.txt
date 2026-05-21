50bcdc0307d4ca9a55256ff49b023dcf30d46d35
dugagss <dugaschool@gmail.com>
2026-05-22 00:19:32 +0530

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,19 +1,20 @@
 
-THIS WEBSITE IS AN EXACT HTML VERSION OF THE PROVIDED SARAS PDF.
+THIS WEBSITE IS AN EXACT HTML VERSION OF THE SARAS 7.0 MANDATORY DISCLOSURE PDF.
 
-ONLY 2 THINGS SHOULD BE CHANGED LATER:
+HOW TO UPDATE:
 
-1. PDF FILES
-Replace files inside:
-pdfs/
-
-KEEP THE SAME FILENAMES.
+1. DOCUMENT LINKS (Section B & C)
+Open: config.js
+Each entry in sectionB and sectionC has a URL as the 3rd item.
+Replace "https://sites.google.com/view/dugagss/home" with the actual URL for each document.
 
 2. YOUTUBE LINK
-Open:
-config.js
+Open: config.js
+Find: youtubeVideo: "https://youtu.be/wDchsz8nmbo"
+Replace ONLY the link inside the quotes with your actual YouTube video link.
 
-Find:
-youtubeVideo: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-
-Replace ONLY the link inside quotes.
+FILES:
+- index.html   → Main page (structure & content)
+- style.css    → All styling
+- script.js    → Populates Section B, C tables and YouTube link
+- config.js    → All configurable links and URLs (edit this file)
--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-
-function buildRows(id,data){
-const tbody=document.getElementById(id);
-
-data.forEach(item=>{
-const tr=document.createElement("tr");
-
-tr.innerHTML=`
-<td>${item[0]}</td>
-<td>${item[1]}</td>
-<td>
-<a href="pdfs/${item[2]}" target="_blank">
-pdfs/${item[2]}
-</a>
-</td>
-`;
-
-tbody.appendChild(tr);
-});
+function buildRows(id, data) {
+  const tbody = document.getElementById(id);
+  data.forEach(function(item) {
+    const tr = document.createElement("tr");
+    tr.innerHTML =
+      "<td>" + item[0] + "</td>" +
+      "<td>" + item[1] + "</td>" +
+      "<td><a href=\"" + item[2] + "\" target=\"_blank\">" + item[2] + "</a></td>";
+    tbody.appendChild(tr);
+  });
 }
 
-buildRows("sectionB",websiteConfig.sectionB);
-buildRows("sectionC",websiteConfig.sectionC);
+buildRows("sectionB", websiteConfig.sectionB);
+buildRows("sectionC", websiteConfig.sectionC);
 
-document.getElementById("youtubeFrame").src=websiteConfig.youtubeVideo;
-document.getElementById("youtubeLink").href=websiteConfig.youtubeVideo;
-document.getElementById("youtubeLink").innerText=websiteConfig.youtubeVideo;
+var ytLink = document.getElementById("youtubeLink");
+ytLink.href = websiteConfig.youtubeVideo;
+ytLink.innerText = websiteConfig.youtubeVideo;
